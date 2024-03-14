a48456dc097b5359101cfd3c37bdfdd619306151
Ayose Suárez Turrumblú (CS) <ayose.suarezturrumbl@gendigital.com>
2024-03-14 18:58:51 +0000

    * Now the state is being retrieved from local storage

--- src/state/reducers/planetsReducer.ts
+++ src/state/reducers/planetsReducer.ts
@@ -1,6 +1,7 @@
 import InitialState, {PlanetsDataState} from "./initialState";
 import PlanetsActions from "../actions/planets.actions";
 import {PlanetsCollection} from "../../models/PlanetsCollection";
+import Storage from "../../utils/Storage";
 
 export interface PlanetsActionType {
     type: keyof typeof PlanetsActions;
@@ -9,7 +10,9 @@ export interface PlanetsActionType {
     ttl?: typeof InitialState.planetsData.ttl;
 }
 
-const PlanetsReducer = (state: PlanetsDataState = InitialState.planetsData, action: PlanetsActionType): PlanetsDataState => {
+const defaultState: PlanetsDataState = Storage.getState()?.planetsData || InitialState.planetsData;
+
+const PlanetsReducer = (state: PlanetsDataState = defaultState, action: PlanetsActionType): PlanetsDataState => {
     switch (action.type) {
         case PlanetsActions.SET_PLANETS:
             return {
