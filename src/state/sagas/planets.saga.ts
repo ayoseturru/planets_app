8d295df65f1d4e30d0f8bef39caac6f4b4be7d4b
Ayose Suárez Turrumblú (CS) <ayose.suarezturrumbl@gendigital.com>
2024-03-14 18:52:32 +0000

    Now the storage is being saved in localstorage

--- src/state/sagas/planets.saga.ts
+++ src/state/sagas/planets.saga.ts
@@ -1,2 +1,13 @@
+import PlanetsActions from "../actions/planets.actions";
+import {PlanetsAppState} from "../reducers/initialState";
+import {call, select, takeEvery} from 'redux-saga/effects';
+import Storage from "../../utils/Storage";
+
+function* saveState() {
+    const state: PlanetsAppState = yield select();
+    yield call(Storage.saveState, state);
+}
+
 export function* planetsSaga() {
+    yield takeEvery([PlanetsActions.SET_PLANETS], saveState);
 }
