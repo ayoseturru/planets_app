d8621343604ec47981c83a8cae30acf38132ec0f
Ayose Suárez Turrumblú (CS) <ayose.suarezturrumbl@gendigital.com>
2024-03-17 13:16:53 +0000

    * Added a simple test for favorites page

---
+++ src/components/pages/favorites/Favorites.test.tsx
@@ -0,0 +1,19 @@
+import {fireEvent, render, screen} from "@testing-library/react";
+import App from "../../../App";
+import {RouterPaths} from "../Main";
+import React from "react";
+import ReferenceLanguageFile from "../../../_locales/en.json";
+
+const expectedDocumentTitle: string = ReferenceLanguageFile.favorites;
+
+describe('Favorites Page', () => {
+    it('should render without crashing', () => {
+        render(<App/>);
+
+        const favoritesLink = screen.getByRole('link', {name: new RegExp(RouterPaths.Favorites.replace("/", ""), 'i')});
+
+        fireEvent.click(favoritesLink);
+        expect(window.location.pathname).toEqual(RouterPaths.Favorites);
+        expect(document.title).toBe(expectedDocumentTitle);
+    });
+});
